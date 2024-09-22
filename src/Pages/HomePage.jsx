419b8f74d745817b2c483b8697028686ced650e0
Ashutosh Jha <105535224+thealonemusk@users.noreply.github.com>
2024-09-22 12:50:06 +0530

    changed the camel_case naming methodology

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,7 +2,7 @@ import  { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import DisplayList from "../Components/DisplayList/DisplayList";
 import Header from "../Components/Header/Header";
-import "../styles/HomePage.css"
+import "../Styles/HomePage.css"
 
 
 const PRIORITY_NAMES = {
