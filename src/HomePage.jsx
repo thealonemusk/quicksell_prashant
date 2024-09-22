9126344b4bf688f06f4cd91d7316b8578223acc8
Prashant <prash2402@gmail.com>
2024-09-22 12:05:21 +0530

    build fix try

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,6 +1,6 @@
 import  { useState, useEffect, useCallback } from "react";
 import axios from "axios";
-import Navbar from "./Components/Header";
+import Navbar from "./Components/Navbar";
 import DisplayList from "./Components/DisplayList";
 import "./Styles/Kanban";
 
