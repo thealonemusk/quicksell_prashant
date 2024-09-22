f89b06103d0c1919b58cb10da1ca4e358cdbb7cb
Prashant <prash2402@gmail.com>
2024-09-22 12:23:26 +0530

    build fix try

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,7 +2,7 @@ import  { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import DisplayList from "../Components/DisplayList/DisplayList";
 import Header from "../Components/Header/Header";
-import "../Styles/Kanban.css"
+import "../Styles/HomePage.css"
 
 const PRIORITY_NAMES = {
   0: "No priority",
